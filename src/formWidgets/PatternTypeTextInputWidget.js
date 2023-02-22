788093d00cedc8dba9334c16c5ad726dcc19a029
Danny Wagner <37207255+wagnerexpositio@users.noreply.github.com>
2023-02-22 13:40:25 +0100

    rückumbau

--- src/formWidgets/PatternTypeTextInputWidget.js
+++ src/formWidgets/PatternTypeTextInputWidget.js
@@ -7,29 +7,17 @@ class PatternTypeTextInputWidget extends Component {
   constructor(props) {
     super(props);
     var startDate = new Date();
-    var startDateTime = new Date();
-    startDateTime = startDateTime.setHours(
-      startDateTime.setMinutes(new Date(), 30),
-      16
-    );
     this.state = {
       value: "",
-      dateTimeValue: "",
       date: "",
-      dateTime: "",
       startDate: startDate,
       setStartDate: startDate,
-      startDateTime: startDateTime,
-      setStartDateTime: startDateTime,
       showDatepicker: props.schema.field_type
     };
   }
 
   componentDidMount() {
     if (this.state.showDatepicker === "date") {
-      this.setStartDate(new Date());
-    }
-    if (this.state.showDatepicker === "datetime-local") {
       this.setStartDate(new Date());
     }
   }
@@ -62,36 +50,6 @@ class PatternTypeTextInputWidget extends Component {
     return date;
   }
 
-  setStartDateTime(dateTime) {
-    this.setState({
-      startDateTime: dateTime
-    });
-
-    var mm = dateTime.getMonth() + 1;
-    var dd = dateTime.getDate();
-    var yy = dateTime.getFullYear();
-    var hours = dateTime.getHours();
-    var minutes = dateTime.getMinutes();
-    dd = dd.toString();
-    mm = mm.toString();
-
-    if (dd.length === 1) {
-      dd = "0" + dd;
-    }
-
-    if (mm.length === 1) {
-      mm = "0" + mm;
-    }
-
-    var date_value = yy + "-" + mm + "-" + dd + " " + hours + ":" + minutes;
-
-    this.setState({
-      dateTimeValue: date_value
-    });
-
-    return dateTime;
-  }
-
   render() {
     const props = this.props;
     const own_this = this;
@@ -104,20 +62,6 @@ class PatternTypeTextInputWidget extends Component {
             dateFormat="dd.MM.yyyy"
             selected={this.state.startDate}
             onChange={date => this.setStartDate(date)}
-            showMonthDropdown
-            showYearDropdown
-          />
-        </div>
-      );
-    } else if (this.state.showDatepicker === "datetime-local") {
-      return (
-        <div>
-          <DatePicker
-            className="form-control"
-            dateFormat="dd.MM.yyyy h:mm"
-            selected={this.state.startDateTime}
-            onChange={date => this.setStartDateTime(date)}
-            showTimeSelect
             showMonthDropdown
             showYearDropdown
           />
